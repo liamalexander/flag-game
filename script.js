da9d16c0e7adee1e2c8e6ed6f5c6cfb77c8bff8d
Liam Alexander <liamalexander7@hotmail.co.uk>
2023-09-30 19:08:38 +0200

    add lives and score

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ const flag = document.querySelector(".flag");
 const guessField = document.querySelector(".guess");
 const checkFlag = document.querySelector(".check-flag");
 const guessOptions = document.querySelector(".guess-options");
+const cleatBtn = document.querySelector(".clear");
+const scoreDisplay = document.querySelector(".score-container");
+const livesHolder = document.querySelector(".lives");
+const scoreHolder = document.querySelector(".score");
 
 let randomNumber;
 let data;
+let score = 0;
+// let lives = 3;
+let lives = ["\u2665", "\u2665", "\u2665"];
+
+// lives.forEach(life => {
+//   life.style.color = "red";
+// });
 
 const getData = async function () {
   const res = await fetch('https://restcountries.com/v3.1/all?fields=name,flags');
@@ -23,6 +34,9 @@ const getData = async function () {
   // guessField.style.display = "block";
   guessField.style.visibility = "visible";
   checkFlag.style.visibility = "visible";
+  cleatBtn.style.visibility = "visible";
+  scoreDisplay.style.visibility = "visible";
+  livesHolder.textContent = `Lives: ${lives.join("  ")}`;
 };
 
 const checkAnswer = function () {
@@ -31,8 +45,13 @@ const checkAnswer = function () {
   // console.log(randomNumber);
   if (guessField.value.toLowerCase() == data[randomNumber].name.common.toLowerCase()) {
     guessField.style.background = "green";
+    score++;
+    scoreHolder.textContent = `Score: ${score}`;
   } else {
     guessField.style.background = "red";
+    // lives--;
+    lives.pop();
+    livesHolder.textContent = `Lives: ${lives.join("  ")}`;
   }
 };
 
@@ -70,23 +89,33 @@ const fillInput = function (e) {
   guessOptions.textContent = "";
 };
 
+const clearInput = function() {
+  guessField.value = "";
+  guessOptions.textContent = "";
+};
+
 guessField.addEventListener("keyup", filterCountries);
 
 // ADD A CORRECT / INCORRECT DISPLAY AND REMOVE THE COLOURS IN BACKGROUND - animation that all screen goes
-//  green or red for a second or so...from top to bottom or left to right?
-// STYLE IT NICER - ADD A NEW FONT FROM GOOGLE
-// MAKE IT MORE OF A GAME
+//  green or red for a second or so...from top to bottom or left to right?  or change the guess background
+//  for only a second or 2 - then back to normal
+//  get new flag automatically on correct guess
+// or make it so you can get next flag, display a message correct or not and click new flag?
+// MAKE IT MORE OF A GAME - add 3 lives and a score (to the top AFTER clicking flag)
+// WHEN CLICK DOWN ON KEYBOARD, SCROLL THROUGH OPTIONS
 // ADD CLEAR BUTTON OR CLEAR AFTER CLICKING CHECK
+// MAKE HEARTS RED AND BIGGER TEXT AND HEARTS - use lives.length to get game over
 // LIMIT # OF LI ITEMS TO 5-8, "AND" BRINGS TOO MANY AND IT FILLS SCREEN
 // CHANGE SIZE ON LAPTOP AND TABLET
-// STYLE BTNS BETTER - BLUE - BORDER RADIUS ETC
-// ADD BORDER-RADIUS TO LEFT OF INPUT AND RIGHT OF CHECK BUTTON - MAKE BOTH BIGGER? - AND BIGGER FONT?
+// STYLE BTNS BETTER - ANIMATE THEM ON CLICK
 
 getFlag.addEventListener("click", getData);
 
 checkFlag.addEventListener("click", checkAnswer);
 
 guessOptions.addEventListener("click", fillInput);
+
+cleatBtn.addEventListener("click", clearInput);
 
 // guessOptions.addEventListener("click", function(e) {
 //   guessField.value = e.target.id;
